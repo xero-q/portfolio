dc5eec6ae33a892f74663d5293020d27139edaa0
Anibal Sanchez <anibalnuma@gmail.com>
2025-07-04 23:34:32 -0400

    Add i18n to footer Github text

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { translations } from "@/lib/i18n";
+import { useLocale } from "@/context/LocaleContext";
 
 const Footer = () => {
+  const { locale } = useLocale();
+  const t = translations[locale];
+
   return (
     <div className="flex flex-col sm:flex-row justify-center items-center mt-4">
       &copy; {new Date().getFullYear()} Aníbal Sánchez Numa
       <span className="hidden sm:inline mx-1">|</span>
       <div className="text-blue-400">
         <a href="https://github.com/xero-q/portfolio" target="_blank">
-          GitHub source code
+          {t.footer.github}
         </a>
       </div>
     </div>
